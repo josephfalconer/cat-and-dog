9e938db026d5983fc583a66c28f390bfd1eb1411
Joe Falconer <joefalconer21@live.co.uk>
2018-05-24 19:59:45 +0100

    Removed local state from Stats and fixed indentation

--- src/components/EndStats.js
+++ src/components/EndStats.js
@@ -5,22 +5,18 @@ class EndStats extends PureComponent {
   static PropTypes = {
     stats: PropTypes.object.isRequired
   }
-
-  state = {
-    stats: []
-  }
-
+  
   render() {
-  const { stats } = this.props;
-  return (
-    <div className="stats">
-      <div className="stats__text">
-        <h4>{`${stats.secondsRemaining === 0 ? 'YOU WIN!' : 'GAME OVER'}`}</h4>
-        <p>{`You lasted for ${60 - stats.secondsRemaining} seconds.`}</p>
-        <p>{`You finished with ${stats.energy} energy.`}</p>
-        <p>{`You ate ${stats.mealsEaten} meals.`}</p>
-      </div>                
-    </div>
+    const { stats } = this.props;
+    return (
+      <div className="stats">
+        <div className="stats__text">
+          <h4>{`${stats.secondsRemaining === 0 ? 'YOU WIN!' : 'GAME OVER'}`}</h4>
+          <p>{`You lasted for ${60 - stats.secondsRemaining} seconds.`}</p>
+          <p>{`You finished with ${stats.energy} energy.`}</p>
+          <p>{`You ate ${stats.mealsEaten} meals.`}</p>
+        </div>                
+      </div>
     );
   }    
 }
